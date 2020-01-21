28b87b6901bd651abeca35e4a75feef3f1f6e2b8
Guilherme de Favere <guilherme.favere@gmail.com>
2020-01-21 13:41:42 -0300

    Create a test that assert if CommentBox component is showed inside App component

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+
+it('shows a comment box', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(<App />, div);
+
+  // Looks inside the div
+  // and checks to see if the CommentBox is in there
+  expect(div.innerHTML).toContain('Box for Comment');
+
+  ReactDOM.unmountComponentAtNode(div);
+});
