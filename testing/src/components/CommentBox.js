b0f844403b5d72face21b5c016dd20897acd72ca
Guilherme de Favere <guilherme.favere@gmail.com>
2020-01-24 13:25:03 -0300

    Implements de textarea element from component: CommentBox

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = e => {
+    this.setState({ comment: e.target.value })
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // TODA - call an action creator
+    // and save the comment
+
+    this.setState({ comment: '' });
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add a Comment</h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment} />
         <div>
           <button>Submit Comment</button>
         </div>
