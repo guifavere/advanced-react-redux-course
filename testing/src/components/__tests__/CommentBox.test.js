708e8cb591f92008d23d88d6bb63825a0783e894
guifavere <guilherme.favere@gmail.com>
2020-06-03 21:46:17 -0300

    Add test that assert if text area is emptied when submitted form at CommentBox component

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -21,4 +21,12 @@ it('has a text area that users can type in', () => {
   wrapped.find('textarea').simulate('change', { target: { value: 'new comment' } });
   wrapped.update();
   expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-})
+});
+
+it('has the text area emptied after submitted form', () => {
+  wrapped.find('textarea').simulate('change', { target: { value: 'new comment' } });
+  wrapped.update();
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
