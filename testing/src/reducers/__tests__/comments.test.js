8d713bf7d52eb16adaf263eed1ac6a36aecb8d4f
guifavere <guilherme.favere@gmail.com>
2020-06-08 20:06:07 -0300

    Add test for unkown type at comments reducer

--- testing/src/reducers/__tests__/comments.test.js
+++ testing/src/reducers/__tests__/comments.test.js
@@ -1,7 +1,7 @@
 import commentsReducer from 'reducers/comments';
 import { SAVE_COMMENT } from 'actions/types';
 
-it('handles actions of type SAVE_COMMENT', () => {
+it('handles action of type SAVE_COMMENT', () => {
   const action = {
     type: SAVE_COMMENT,
     payload: 'New comment'
@@ -10,4 +10,10 @@ it('handles actions of type SAVE_COMMENT', () => {
   const newState = commentsReducer([], action);
 
   expect(newState).toEqual(['New comment']);
+});
+
+it('handles action with unkown type', () => {
+  const newState = commentsReducer([], { type: 'BLAKDSDKO3' });
+
+  expect(newState).toEqual([]);
 });
