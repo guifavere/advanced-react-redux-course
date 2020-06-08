aebf20a55ec4d8337b79e1e5a3ad58de55327bc7
guifavere <guilherme.favere@gmail.com>
2020-06-08 19:54:05 -0300

    Fix the: CommentBox component tests

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from 'actions';
 
 class CommentBox extends Component {
   state = { comment: '' };
@@ -10,9 +13,7 @@ class CommentBox extends Component {
   handleSubmit = e => {
     e.preventDefault();
 
-    // TODA - call an action creator
-    // and save the comment
-
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: '' });
   }
 
@@ -29,4 +30,4 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
