d6d1f8e5e27e40570480cdc96dd5f2e1917c9a13
guifavere <guilherme.favere@gmail.com>
2020-06-09 18:43:36 -0300

    Add button that fetch comments from api

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import reduxPromise from 'redux-promise';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducers from './reducers';
 
 export default ({ children, initialState = {} }) => {
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(reduxPromise)
+  );
+
   return (
-    <Provider store={createStore(reducers, initialState)}>
+    <Provider store={store}>
       {children}
     </Provider>
   );
