952ce62dd3e532945c1e9edfcd906f1f5551df0a
guifavere <guilherme.favere@gmail.com>
2020-06-09 12:52:17 -0300

    Create test that assert the component: CommentList

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -4,10 +4,10 @@ import { createStore } from 'redux';
 
 import reducers from './reducers';
 
-export default (props) => {
+export default ({ children, initialState = {} }) => {
   return (
-    <Provider store={createStore(reducers, {})}>
-      {props.children}
+    <Provider store={createStore(reducers, initialState)}>
+      {children}
     </Provider>
   );
 };
---
+++ testing/src/components/__tests__/CommentList.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import CommentList from 'components/CommentList';
+import Root from 'Root';
+
+let wrapped;
+
+beforeEach(() => {
+  const initialState = {
+    comments: ['Comment 1', 'Comment 2'],
+  };
+
+  wrapped = mount(
+    <Root initialState={initialState}>
+      <CommentList />
+    </Root>
+  );
+});
+
+it('creates one LI per comment', () => {
+  expect(wrapped.find('li').length).toEqual(2);
+});
