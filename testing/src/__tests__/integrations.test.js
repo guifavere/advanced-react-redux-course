9acc99f4c5580d233e29e0ac1cb73b2ab9b79cd2
guifavere <guilherme.favere@gmail.com>
2020-06-10 12:48:01 -0300

    Improve the integrations test

--- testing/src/__tests__/integrations.test.js
+++ testing/src/__tests__/integrations.test.js
@@ -20,17 +20,22 @@ afterEach(() => {
   moxios.uninstall();
 });
 
-it('can fetch a list of comments and display them', async () => {
-  // attempt to render the entire app
+it('can fetch a list of comments and display them', done => {
   const wrapped = mount(
     <Root>
       <App />
     </Root>
   );
 
-  // find the 'fetchComments' button and click it
   wrapped.find('.fetch-comments').simulate('click');
 
-  // expect to find a list of comments!
-  expect(wrapped.find('li').length).toEqual(2);
+  moxios.wait(() => {
+    wrapped.update();
+
+    expect(wrapped.find('li').length).toEqual(2);
+
+    done();
+
+    wrapped.unmount();
+  });
 });
