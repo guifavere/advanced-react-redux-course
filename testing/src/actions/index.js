04e1f82447ba2a3be4d76cb9f19037706a22e2eb
guifavere <guilherme.favere@gmail.com>
2020-06-10 13:13:58 -0300

    Create auth reducer

--- testing/src/actions/index.js
+++ testing/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 
 export function saveComment(comment) {
   return {
@@ -15,5 +15,12 @@ export function fetchComments() {
   return {
     type: FETCH_COMMENTS,
     payload: response,
+  };
+}
+
+export function changeAuth(isLoggedIn) {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn,
   };
 }
