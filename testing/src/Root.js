c47326f99f21c9df525bd3a921b858f73ca24765
guifavere <guilherme.favere@gmail.com>
2020-06-17 20:23:39 -0300

    Create the middleware: async

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import reduxPromise from 'redux-promise';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import async from './middlewares/async';
 import reducers from './reducers';
 
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    applyMiddleware(async)
   );
 
   return (
