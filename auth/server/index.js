dfc777fa1c6c29dd34dafa81a44a197f36a9dc4b
guifavere <guilherme.favere@gmail.com>
2020-06-24 21:09:44 -0300

    Setup the database connection

--- auth/server/index.js
+++ auth/server/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const moongose = require('mongoose');
 
 const app = express();
 const router = require('./router');
+
+// db setup
+moongose.connect('mongodb://localhost:27017/auth');
 
 // app setup
 app.use(morgan('combined'));
