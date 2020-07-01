313282ff8cf8bfb06e3924412c2bcd0f1d6e64fb
guifavere <guilherme.favere@gmail.com>
2020-06-30 21:30:28 -0300

    Config the redux form at Signup component

--- auth/client/src/components/auth/Signup.js
+++ auth/client/src/components/auth/Signup.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 class Signup extends Component {
+  onSubmit = (formProps) => {
+    console.log({ formProps });
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <fieldset>
           <label>Email</label>
+          <Field autoComplete="none" name="email" type="text" component="input" />
         </fieldset>
         <fieldset>
           <label>Password</label>
+          <Field autoComplete="none" name="password" type="password" component="input" />
         </fieldset>
+        <button type="submit">Sign up</button>
       </form>
     );
   };
 };
 
-export default Signup;
+export default reduxForm({ form: 'signup' })(Signup);
