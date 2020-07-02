acd66aeabacc8db9eb3aabfc2612ff285f9c39a4
guifavere <guilherme.favere@gmail.com>
2020-07-02 17:05:43 -0300

    Create signup action

--- auth/client/src/components/auth/Signup.js
+++ auth/client/src/components/auth/Signup.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions';
 
 class Signup extends Component {
-  onSubmit = (formProps) => {
-    console.log({ formProps });
+  onSubmit = formProps => {
+    this.props.signup(formProps);
   }
 
   render() {
@@ -25,4 +29,7 @@ class Signup extends Component {
   };
 };
 
-export default reduxForm({ form: 'signup' })(Signup);
+export default compose(
+  connect(null, actions),
+  reduxForm({ form: 'signup' })
+)(Signup);
