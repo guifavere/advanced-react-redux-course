e049c27f2393d75009e388179911f9d323a904bc
guifavere <guilherme.favere@gmail.com>
2020-07-13 19:10:04 -0300

    Improve the component: Header

--- auth/client/src/components/Header.js
+++ auth/client/src/components/Header.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import './HeaderStyle.css';
 
 class Header extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <div>
+          <Link to="/signout">Sign out</Link>
+          <Link to="/feature">Feature</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to="/signup">Sign up</Link>
+          <Link to="/signin">Sign in</Link>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
+      <div className="header">
         <Link to="/">Redux Auth</Link>
-        <Link to="/signup">Sign up</Link>
-        <Link to="/signin">Sign in</Link>
-        <Link to="/signout">Sign out</Link>
-        <Link to="/feature">Feature</Link>
+        {this.renderLinks()}
       </div>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
