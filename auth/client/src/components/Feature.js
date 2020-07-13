55fe6820136e6088bdd8f9c7c63aa62bf4291b59
guifavere <guilherme.favere@gmail.com>
2020-07-13 18:38:17 -0300

    Only show feature component for logged in users

--- auth/client/src/components/Feature.js
+++ auth/client/src/components/Feature.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import requireAuth from './requireAuth';
+
 class Feature extends Component {
   render() {
     return <div>This is the feature!</div>;
   }
 }
 
-export default Feature;
+export default requireAuth(Feature);
